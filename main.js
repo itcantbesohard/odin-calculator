d1d79803bfbedd6a0ca95885db2cb1b97a2fa08e
Michał Baran <mbaran@polonez-plus.com.pl>
2026-03-16 12:01:11 +0100

    Add AC, CE and operate buttons handle

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const numberOne = 0;
+const numberTwo = 0;
+let entered = [];
+const ops = "+-*/";
+
 const display = document.querySelector(`.display p`);
 display.textContent = "0";
 
@@ -5,6 +10,43 @@ const buttons = document.querySelector(`.buttons`);
 
 buttons.addEventListener("click", (e) => {
     e.preventDefault();
+    if (!e.target.classList.contains("btn")) return;
     if (display.textContent === "0") display.textContent = "";
-    display.textContent += e.target.textContent;
+    if (e.target.id === "AC") {
+        clearAll();
+        return;
+    }
+    if (e.target.id === "CE") {
+        clearEntry();
+        return;
+    }
+    if (e.target.classList.contains("btn-operate")) {
+        const last = entered.at(-1);
+
+        if (ops.includes(last)) {
+            entered[entered.length - 1] = e.target.textContent;
+        } else {
+            entered.push(e.target.textContent);
+        }
+
+        display.textContent = entered.join("");
+        return;
+    }
+
+    entered.push(e.target.textContent);
+    display.textContent = entered.join("");
 });
+
+function clearEntry() {
+    entered.pop();
+    display.textContent = entered.join("");
+    if (entered.length === 0) display.textContent = 0;
+}
+
+
+function clearAll() {
+    display.textContent = 0;
+    numberOne = 0;
+    numberTwo = 0;
+    entered = [];
+}
