3e3a956fb0c8ac3dc4decd640910a2f6d603012f
Michał Baran <michalbaran@outlook.com>
2026-03-20 20:19:10 +0100

    Add keydown handling

--- main.js
+++ main.js
@@ -11,32 +11,90 @@ buttons.addEventListener("click", (e) => {
 
     if (btn.dataset.number) {
         calc.appendDigit(btn.dataset.number);
+        calc.updateDisplay();
+        return;
     }
 
     if (btn.dataset.action === "AC") {
         calc.clear();
+        calc.updateDisplay();
+        return;
     }
 
     if (btn.dataset.action === "CE") {
         calc.delete();
+        calc.updateDisplay();
+        return;
     }
 
     if (btn.dataset.action === "decimal") {
         calc.addDecimal();
+        calc.updateDisplay();
+        return;
     }
 
     if (btn.dataset.action === "percent") {
         calc.percent();
+        calc.updateDisplay();
+        return;
     }
 
     if (btn.dataset.operator) {
         calc.applyOperator(btn.textContent);
+        calc.updateDisplay();
+        return;
     }
 
     if (btn.dataset.action === "equals") {
         calc.equals();
+        calc.updateDisplay();
+        return;
     }
 
-    calc.updateDisplay();
-    return;
 });
+
+document.addEventListener("keydown", (e) => {
+    const key = e.key;
+
+    if (!isNaN(key)) {
+        calc.appendDigit(key);
+        calc.updateDisplay();
+        return;
+    }
+
+    if (["+", "-", "/", "*"].includes(key)) {
+        calc.applyOperator(key);
+        calc.updateDisplay();
+        return;
+    }
+
+    if (key === "." || key === ",") {
+        calc.addDecimal();
+        calc.updateDisplay();
+        return;
+    }
+
+    if (key === "%") {
+        calc.percent();
+        calc.updateDisplay();
+        return;
+    }
+
+    if (key === "Enter" || key === "=") {
+        calc.equals();
+        calc.updateDisplay();
+        return;
+    }
+
+    if (key === "Backspace") {
+        calc.delete;
+        calc.updateDisplay();
+        return;
+    }
+
+    if (key === "Escape") {
+        calc.clear();
+        calc.updateDisplay();
+        return;
+    }
+})
